b7b7add3b30a172954b74837b170d5569f88b9b7
unknown <fluorescence.t@gmail.com>
2024-04-23 22:01:08 +0800

    3

--- ml.py
+++ ml.py
@@ -21,10 +21,11 @@ class Base:
 class Linear:
     def __init__(self, in_features, out_features, bias=False):
         self.weight = [
-            [random.random() for _ in range(in_features)] for _ in range(out_features)
+            [Tensor(random.random()) for _ in range(in_features)] for _ in range(out_features)
         ]
+        print(self.weight)
         if bias == True:
-            self.bias = [random.random() for _ in range(out_features)]
+            self.bias = [Tensor(random.random() for _ in range(out_features))]
         else:
             self.bias = None
 
@@ -46,3 +47,108 @@ class Linear:
             print(self.bias)
         else:
             print(self.weight)
+
+
+import math
+
+
+class Tensor:
+    def __init__(self, data, _children=(), _op=""):
+        if (isinstance(data, list)):
+            return [Tensor(i) for i in data]
+        self.data = data
+        self._prev = set(_children)
+        self._backward = lambda: None
+        self._op = _op
+        self.grad = 0.0
+
+    def __repr__(self):
+        return f"Tensor[data = {self.data}]"
+
+    def __add__(self, other):
+        if isinstance(other, Tensor) == False:
+            other = Tensor(other)
+        out = Tensor(self.data + other.data, (self, other), "+")
+
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __mul__(self, other):
+        if isinstance(other, Tensor) == False:
+            other = Tensor(other)
+        out = Tensor(self.data * other.data, (self, other), "*")
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float))
+        out = Tensor(self.data**other, (self,), f"**{other}")
+
+        def _backward():
+            self.grad += (other * self.data ** (other - 1)) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __neg__(self):
+        return self * -1
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        return self * other**-1
+
+    def tanh(self):
+        x = self.data
+        out = (math.exp(2.0 * x) - 1) / (math.exp(2.0 * x) + 1)
+        out = Tensor(out, (self,), "tanh")
+
+        def _backward():
+            self.grad += (1 - out.data**2) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def exp(self):
+        x = self.data
+        out = Tensor(math.exp(x), (self,), "exp")
+
+        def _backward():
+            self.grad += out.data * out.grad
+
+        out._backward = _backward
+        return out
+
+    def backward(self):
+        self.grad = 1.0
+
+        nodes = []
+        added_nodes = set()
+
+        def get_child_nodes(v):
+            if v not in added_nodes:
+                added_nodes.add(v)
+                for child in v._prev:
+                    get_child_nodes(child)
+                nodes.append(v)
+
+        get_child_nodes(self)
+
+        for node in reversed(nodes):
+            node._backward()
+            
+    def shape():
+        return None
